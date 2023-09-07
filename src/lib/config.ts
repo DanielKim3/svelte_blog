dd2282d21a65818234f739e08bc79b12c470cabe
kimwoojin <wjinkim21@naver.com>
2023-09-08 01:28:09 +0900

    svelte blog

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'DanielKim3';
+export const description = 'Sveltekit blog for Daniel';
+export const url = dev ? 'http://127.0.0.1:5173/' : 'htt://danielkim:xyz';
